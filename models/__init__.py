488ebf784773cd3c669021ad9bb88728a09233b2
Edward Osorno <edward-1094@hotmail.com>
2020-02-19 04:25:54 +0000

    add

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+This module initialize the storage instance and reload the objects
+"""
 from models import base_model
 from models import user
 from models.engine.file_storage import FileStorage
